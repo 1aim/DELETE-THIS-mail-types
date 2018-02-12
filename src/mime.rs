96eedb6dce7311dd4e849eaba92b5a927c7400ad
Philipp Korber <philippkorber@gmail.com>
2018-02-12 10:43:23 +0100

    chore(mime): updated boundary generation chars

--- src/mime.rs
+++ src/mime.rs
@@ -8,39 +8,60 @@ use mheaders::components::MediaType;
 /// write a random sequence of chars valide for and boundary to the output buffer
 ///
 /// Note that it might be required to quote the boundary.
+///
 /// The boundary (excluding quotations) will start with `"=_^"` which is neither
 /// valid for base64 nor quoted-printable encoding.
 ///
-/// The boundary will be picked from ascii `VCHAR`'s (us-ascii >= 33 and <= 126) but
-/// following `VCHAR`'s are excluded `'"'`, `'-'` and `'\\'`.
+/// The boundary will be 66 chars long, this is so that if a boundary parameter is
+/// placed on it's own line it won't be more then 78 chars. (66 chars boundary,
+/// + 2 chars quotation + 9 chars for 'boundary=' + 1 char because of `\r\n<WS>`
+/// == 78 chars)
+///
+/// The remaining characters will be picked based one the grammar defined in rfc2046,
+/// whichs relevant part is:
+///
+/// ```BNF
+/// boundary := 0*69<bchars> bcharsnospace
+/// bchars := bcharsnospace / " "
+/// bcharsnospace := DIGIT / ALPHA / "'" / "(" / ")" /
+///                  "+" / "_" / "," / "-" / "." /
+///                  "/" / ":" / "=" / "?"
+/// ```
+///
 pub fn create_random_boundary() -> String {
+
+
     //the maximal boundary with wich " boundary=\"...\"" fits into 78 chars line length limit
     const MULTIPART_BOUNDARY_LENGTH: usize = 66;
     // boundary chars based on rfc2046, excluding " "
     // (it can be used in any place _except_ the last)
-    debug_assert!(4 <= MULTIPART_BOUNDARY_LENGTH && MULTIPART_BOUNDARY_LENGTH <= 70);
+    debug_assert!(1 <= MULTIPART_BOUNDARY_LENGTH && MULTIPART_BOUNDARY_LENGTH <= 70);
     static CHARS: &[char] = &[
-        '\'', '(',
-        ')',      '+', ',',      '.', '/', '0',
-        '1', '2', '3', '4', '5', '6', '7', '8',
-        '9', ':',           '=',      '?',
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-        'Y', 'Z',                     '_',
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-        'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-        'y', 'z',
+        ' ',                               '\'',
+        '(', ')',      '+', ',', '-', '.', '/',
+        '0', '1', '2', '3', '4', '5', '6', '7',
+        '8', '9', ':',           '=',      '?',
+             'A', 'B', 'C', 'D', 'E', 'F', 'G',
+        'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+        'X', 'Y', 'Z',                     '_',
+             'a', 'b', 'c', 'd', 'e', 'f', 'g',
+        'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+        'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+        'x', 'y', 'z',
     ];
 
     // we add =_^ to the boundary, as =_^ is neither valid in base64 nor quoted-printable
     let mut out = String::with_capacity(MULTIPART_BOUNDARY_LENGTH);
     out.push_str("=_^");
     let mut rng = rand::thread_rng();
-    for _ in 3..MULTIPART_BOUNDARY_LENGTH {
+    for _ in 3..(MULTIPART_BOUNDARY_LENGTH-1) {
         out.push( CHARS[ rng.gen_range( 0, CHARS.len() )] )
     }
+
+    debug_assert!(CHARS[0] == ' ');
+    let last_idx = rng.gen_range(1, CHARS.len());
+    out.push(CHARS[last_idx]);
     out
 }
 
@@ -88,14 +109,14 @@ mod test {
             let out = create_random_boundary();
 
             for ch in out[1..out.len()-1].chars() {
-                assert!(ch as u32 >= 33);
+                assert!(ch as u32 >= 32);
                 assert!(ch as u32 <= 126);
-                assert_ne!(ch, ' ');
                 assert_ne!(ch, '\t');
                 assert_ne!(ch, '\\');
                 assert_ne!(ch, '"');
             }
 
+            assert_ne!(out.as_bytes()[out.len()-1], b' ');
         }
     }
 }
